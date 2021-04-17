572f70b289573a6f90ca3c8c0655d0a320988c60
AlexeyMarg <65703985+AlexeyMarg@users.noreply.github.com>
2021-04-17 19:12:24 +0300

    nn parameters

---
+++ main.py
@@ -0,0 +1,8 @@
+import  neural_network
+
+# number of input nodes is a number of pixels
+input_nodes = 784
+hidden_nodes = 100
+# each output node corresponds to particular number
+output_nodes = 10
+learning_rate = 0.3
