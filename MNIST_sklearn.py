c60cc024f62a434f6d1ae1623a3de8620aa96bfc
AlexeyMarg <65703985+AlexeyMarg@users.noreply.github.com>
2021-07-23 13:39:51 +0300

    show random number

--- MNIST_sklearn.py
+++ MNIST_sklearn.py
@@ -1,5 +1,17 @@
 from sklearn.datasets import fetch_openml
+import numpy as np
+import matplotlib
+import matplotlib.pyplot as plt
 
 mnist = fetch_openml('mnist_784')
 X, y = mnist['data'], mnist['target']
-print(X.shape, y.shape)
+
+
+def show_some_digit(X):
+    sample = np.random.randint(X.shape[0])
+    digit_image = X[sample].reshape(28, 28)
+    plt.imshow(digit_image, cmap=matplotlib.cm.binary, interpolation='nearest')
+    plt.axis('off')
+    plt.show()
+
+show_some_digit(X)
