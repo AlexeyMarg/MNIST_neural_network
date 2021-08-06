0e6da57676d1a4be97e2c5c9e095fc072844b1ee
AlexeyMarg <65703985+AlexeyMarg@users.noreply.github.com>
2021-08-06 11:57:24 +0300

    function for random forest

--- MNIST_sklearn.py
+++ MNIST_sklearn.py
@@ -1,5 +1,5 @@
 from sklearn.datasets import fetch_openml
-from sklearn.model_selection import cross_val_score
+from sklearn.model_selection import cross_val_score, cross_val_predict
 from sklearn.metrics import confusion_matrix, accuracy_score, precision_score, recall_score, f1_score, roc_curve
 from sklearn.linear_model import SGDClassifier
 from sklearn.ensemble import RandomForestClassifier
@@ -18,84 +18,14 @@ def show_some_digit(X):
 
 #show_some_digit(X)
 
-X_train, X_test, y_train, y_test = X[:60000], X[60000:], y[:60000], y[60000:]
-shuffle_index = np.random.permutation(60000)
-X_train, y_train = X[shuffle_index], y[shuffle_index]
-
 def prepare_data():
+    print('Loading of data')
     mnist = fetch_openml('mnist_784')
     X, y = mnist['data'].astype(np.int8), mnist['target'].astype(np.int8)
     X_train, X_test, y_train, y_test = X[:60000], X[60000:], y[:60000], y[60000:]
     shuffle_index = np.random.permutation(60000)
     X_train, y_train = X[shuffle_index], y[shuffle_index]
     return  X_train, X_test, y_train, y_test
-
-def use_SGDClassifier(X_train, X_test, y_train, y_test):
-    sgd_clf = SGDClassifier()
-    sgd_clf.fit(X_train, y_train)
-    y_train_predict = cross_val_score(sgd_clf, X_train, y_train, cv=3)
-    print('SGDClassifier results:')
-    print('Cross validation score:')
-    print(y_train_predict)
-    y_test_predict = sgd_clf.predict(X_test)
-    conf_matrix = confusion_matrix(y_test, y_test_predict)
-    print('Confusion matrix:')
-    print(conf_matrix)
-    accuracy = accuracy_score(y_test, y_test_predict)
-    print('Accuracy: ', accuracy)
-    precision = precision_score(y_test, y_test_predict)
-    print('Precision: ', precision)
-    recall = recall_score(y_test, y_test_predict)
-    print('Recall: ', recall)
-    clf_f1_score = f1_score(y_test, y_test_predict)
-    print('f1 score: ', clf_f1_score, '\n\n')
-    return sgd_clf
-
-def use_RandomForestClassifier(X_train, X_test, y_train, y_test):
-    rf_clf = RandomForestClassifier()
-    rf_clf.fit(X_train, y_train)
-    y_train_predict = cross_val_score(rf_clf, X_train, y_train, cv=3)
-    print('RandomForestClassifier results:')
-    print('Cross validation score:')
-    print(y_train_predict)
-    y_test_predict = rf_clf.predict(X_test)
-    conf_matrix = confusion_matrix(y_test, y_test_predict)
-    print('Confusion matrix:')
-    print(conf_matrix)
-    accuracy = accuracy_score(y_test, y_test_predict)
-    print('Accuracy: ', accuracy)
-    precision = precision_score(y_test, y_test_predict)
-    print('Precision: ', precision)
-    recall = recall_score(y_test, y_test_predict)
-    print('Recall: ', recall)
-    clf_f1_score = f1_score(y_test, y_test_predict)
-    print('f1 score: ', clf_f1_score, '\n\n')
-    return rf_clf
-
-
-X_train, X_test, y_train, y_test = prepare_data()
-use_SGDClassifier(X_train, X_test, y_train, y_test)
-
-
-'''
-y_train_5 = (y_train == 5)
-y_test_5 = (y_test == 5)
-
-
-sgd_clf = SGDClassifier( random_state=42)
-sgd_clf.fit(X_train, y_train_5)
-y_pred_5 = sgd_clf.predict(X_test)
-y_train_5_predict = cross_val_predict(sgd_clf, X_train, y_train_5, cv=3)
-conf_matrix = confusion_matrix(y_train_5, y_train_5_predict)
-print('confusion matrix:\n', conf_matrix)
-print('precission: ', precision_score(y_train_5, y_train_5_predict))
-print('recall: ', recall_score(y_train_5, y_train_5_predict))
-print('f1 score: ', f1_score(y_train_5, y_train_5_predict))
-
-y_scores = cross_val_predict(sgd_clf, X_train, y_train_5, cv=3, method='decision_function')
-
-
-fpr, tpr, thresholds = roc_curve(y_train_5, y_scores)
 
 def plot_roc_curve(fpr, tpr, label=None):
     plt.plot(fpr, tpr, linewidth=2, label=label)
@@ -104,13 +34,67 @@ def plot_roc_curve(fpr, tpr, label=None):
     plt.xlabel('False positive rate')
     plt.ylabel('True positive rate')
 
-forrest_clf = RandomForestClassifier(random_state=42)
-y_probas_forrest = cross_val_predict(forrest_clf, X_train, y_train_5, cv=3, method='predict_proba')
-y_scores_forrest = y_probas_forrest[:, 1]
+def get_tpr_fpr(clf, X_test, y_test):
+    y_scores = cross_val_predict(clf, X_test, y_test, cv=3, method='decision_function')
+    fpr, tpr, thresholds = roc_curve(y_test, y_scores)
+    return tpr, fpr
 
-fpr_forrest, tpr_forrest, thresholds_forrest = roc_curve(y_train_5, y_scores_forrest)
-plt.plot(fpr, tpr, 'b:', label='GSD')
-plot_roc_curve(fpr_forrest, tpr_forrest, 'Random forrest')
+def use_SGDClassifier(X_train, X_test, y_train, y_test):
+    sgd_clf = SGDClassifier()
+    print('\n\nStsrt fit SGD\n\n')
+    sgd_clf.fit(X_train, y_train)
+    y_train_cv_score = cross_val_score(sgd_clf, X_train, y_train, cv=3)
+    print('SGDClassifier results for test data:')
+    print('Cross validation score:')
+    print(y_train_cv_score)
+    print('\nTest data results')
+    y_test_predict = sgd_clf.predict(X_test)
+    conf_matrix = confusion_matrix(y_test, y_test_predict)
+    print('Confusion matrix:')
+    print(conf_matrix)
+    accuracy = accuracy_score(y_test, y_test_predict)
+    print('Accuracy: ', accuracy)
+    precision = precision_score(y_test, y_test_predict, average=None)
+    print('Precision: ', precision)
+    recall = recall_score(y_test, y_test_predict, average=None)
+    print('Recall: ', recall)
+    clf_f1_score = f1_score(y_test, y_test_predict, average=None)
+    print('f1 score: ', clf_f1_score, '\n\n')
+    return sgd_clf
+
+def use_RandomForestClassifier(X_train, X_test, y_train, y_test):
+    rf_clf = RandomForestClassifier()
+    print('\n\nStsrt fit RF\n\n')
+    rf_clf.fit(X_train, y_train)
+    y_train_cv = cross_val_score(rf_clf, X_train, y_train, cv=3)
+    print('RandomForestClassifier results for test data:')
+    print('Cross validation score:')
+    print(y_train_cv)
+    print('\nTest data results')
+    y_test_predict = rf_clf.predict(X_test)
+    conf_matrix = confusion_matrix(y_test, y_test_predict)
+    print('Confusion matrix:')
+    print(conf_matrix)
+    accuracy = accuracy_score(y_test, y_test_predict)
+    print('Accuracy: ', accuracy)
+    precision = precision_score(y_test, y_test_predict, average=None)
+    print('Precision: ', precision)
+    recall = recall_score(y_test, y_test_predict, average=None)
+    print('Recall: ', recall)
+    clf_f1_score = f1_score(y_test, y_test_predict, average=None)
+    print('f1 score: ', clf_f1_score, '\n\n')
+    return rf_clf
+
+
+X_train, X_test, y_train, y_test = prepare_data()
+
+sgd_clf = use_SGDClassifier(X_train, X_test, y_train, y_test)
+sgd_tpr, sgd_fpr = get_tpr_fpr(sgd_clf, X_test, y_test)
+
+rf_clf = use_SGDClassifier(X_train, X_test, y_train, y_test)
+rf_tpr, rf_fpr = get_tpr_fpr(rf_clf, X_test, y_test)
+
+plt.plot(sgd_fpr, sgd_tpr, 'b:', label='GSD')
+plot_roc_curve(rf_fpr, rf_tpr, 'Random forrest')
 plt.legend(loc='lower right')
 plt.show()
-'''
