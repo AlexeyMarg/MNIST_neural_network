2864eeaacce56146b160a55ca3715adeeec7933a
AlexeyMarg <65703985+AlexeyMarg@users.noreply.github.com>
2021-08-08 22:42:07 +0300

    use_KNeighЬorsClassifier

--- MNIST_sklearn.py
+++ MNIST_sklearn.py
@@ -1,8 +1,9 @@
 from sklearn.datasets import fetch_openml
-from sklearn.model_selection import cross_val_score, cross_val_predict
-from sklearn.metrics import confusion_matrix, accuracy_score, precision_score, recall_score, f1_score, roc_curve
+from sklearn.model_selection import cross_val_score
+from sklearn.metrics import confusion_matrix, accuracy_score, precision_score, recall_score, f1_score
 from sklearn.linear_model import SGDClassifier
 from sklearn.ensemble import RandomForestClassifier
+from sklearn.neighbors import KNeighborsClassifier
 import numpy as np
 import matplotlib
 import matplotlib.pyplot as plt
@@ -52,7 +53,7 @@ def use_SGDClassifier(X_train, X_test, y_train, y_test):
 
 def use_RandomForestClassifier(X_train, X_test, y_train, y_test):
     rf_clf = RandomForestClassifier()
-    print('\n\nStsrt fit RF\n\n')
+    print('\n\nStart fit RF\n\n')
     rf_clf.fit(X_train, y_train)
     y_train_cv = cross_val_score(rf_clf, X_train, y_train, cv=3)
     print('RandomForestClassifier results for test data:')
@@ -73,6 +74,29 @@ def use_RandomForestClassifier(X_train, X_test, y_train, y_test):
     print('f1 score: ', clf_f1_score, '\n\n')
     return rf_clf
 
+def use_KNeighЬorsClassifier(X_train, X_test, y_train, y_test):
+    kn_clf = KNeighborsClassifier(weights='distance', n_neighbors=50)
+    print('\n\nStart fit KNeighborsClassifier\n\n')
+    kn_clf.fit(X_train, y_train)
+    y_train_cv = cross_val_score(kn_clf, X_train, y_train, cv=3)
+    print('KNeighborsClassifier results for test data:')
+    print('Cross validation score:')
+    print(y_train_cv)
+    print('\nTest data results')
+    y_test_predict = kn_clf.predict(X_test)
+    conf_matrix = confusion_matrix(y_test, y_test_predict)
+    print('Confusion matrix:')
+    print(conf_matrix)
+    accuracy = accuracy_score(y_test, y_test_predict)
+    print('Accuracy: ', accuracy)
+    precision = precision_score(y_test, y_test_predict, average=None)
+    print('Precision: ', precision)
+    recall = recall_score(y_test, y_test_predict, average=None)
+    print('Recall: ', recall)
+    clf_f1_score = f1_score(y_test, y_test_predict, average=None)
+    print('f1 score: ', clf_f1_score, '\n\n')
+    return kn_clf
+
 def prediction(clf, X):
     y = clf.predict()
     return y
@@ -84,4 +108,6 @@ X_train, X_test, y_train, y_test = prepare_data()
 sgd_clf = use_SGDClassifier(X_train, X_test, y_train, y_test)
 
 rf_clf = use_RandomForestClassifier(X_train, X_test, y_train, y_test)
+
+kn_clf = use_KNeighЬorsClassifier(X_train, X_test, y_train, y_test)
 
